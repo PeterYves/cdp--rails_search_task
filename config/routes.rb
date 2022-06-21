a70e7a5f8a1198e4818126ae1b6a362c922407db
Peter Yves <52608664+PeterYves@users.noreply.github.com>
2022-06-21 08:36:08 +0200

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
-Rails.application.routes.draw do
-  root 'tasks#index'
-  resources :tasks
+root 'tasks#index'
+resources :tasks do
+  collection do
+    match 'search' => 'tasks#index', via: [:get, :post], as: :search
+  end
 end
